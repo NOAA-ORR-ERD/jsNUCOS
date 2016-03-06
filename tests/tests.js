e4df4805bba3d71a5f1e628f10d6f7f5ded6d8b6
Nathan Wilkins <nathan.wilkins@noaa.gov>
2016-03-06 14:07:59 -0800

    added tests for includes in different contexts and mass/volume conversions

--- tests/tests.js
+++ tests/tests.js
@@ -1,9 +1,20 @@
-var requirejs = require('requirejs');
 var assert = require('assert');
-nucos = requirejs('/../nucos.js');
 
+describe('nucos', function(){
+    it('should import with amd', function(){
+        var requirejs = require('requirejs');
+        var n_test = requirejs('/../nucos.js');
+        assert.notEqual(n_test, undefined);
+    });
 
-describe('sexagesimal2decimal', function(){
+    it('should import with commonjs', function(){
+        var n_test = require('../nucos');
+        assert.notEqual(n_test, undefined);
+    });
+});
+
+var nucos = require('../nucos');
+describe('nucos.sexagesimal2decimal', function(){
     it('should convert lat long into decimal', function(){
         var lon = "24° 43' 30.16\"";
         var lat = "58° 44' 43.97\"";
@@ -26,5 +37,18 @@ describe('sexagesimal2decimal', function(){
 
         assert.equal(nucos.sexagesimal2decimal(lon), -24.72504444);
         assert.equal(nucos.sexagesimal2decimal(lat), -58.74554722);
+    });
+});
+
+describe('nucos.convert', function(){
+    it('should convert same unit density', function(){
+        assert.equal(nucos.convert("Density", "API degree", "kg/m^3", 10), 999.13);
+    });
+});
+
+describe('nocos.OilQuantityConverter', function(){
+    it('should convert oil quantity between volume and mass', function(){
+        var oc = new nucos.OilQuantityConverter();
+        assert.equal(oc.Convert(50, "tons", 10, "API degree", "cubic meters"), 45.39873389849169);
     });
 });
